c53f2986c3a29fc85b491d2945cb0f8e1ad84445
Davi Samuel Da Silva <davidev.front@gmail.com>
2023-04-20 12:54:20 -0300

    trocado cards por tabela

--- src/Components/Pages/Lojas/CardLojas.jsx
+++ src/Components/Pages/Lojas/CardLojas.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import BodyContainer from "../../style/Body-styled.jsx";
-import Card from "../../style/Card-style.jsx";
+import Table from "../../style/Table-style.jsx";
 import { Link } from "react-router-dom";
 
 
@@ -16,32 +16,48 @@ function ListaDeLoja() {
   return (
     <>
       <BodyContainer> <h1>Opções de lojas</h1></BodyContainer>
-
-      {formValuesList.length !== 0 ? (
-        <BodyContainer>
-          {formValuesList.map((formValues, index) => (
-            <Card key={index}>
-              <h2>{formValues.nome}</h2>
-              <p>Razão social: {formValues.razaoSocial}</p>
-              <p>CNPJ: {formValues.cnpj}</p>
-              <p>Nome Fantasia: {formValues.nomeFantasia}</p>
-              <p>E-mail: {formValues.email}</p>
-              <p>Telefone: {formValues.telefone}</p>
-              <p>Celular: {formValues.celular}</p>
-              <p>{formValues.endereco} - n° {formValues.numero}</p>
-              <p>Cidade:{formValues.cidade}</p>
-              <p> Bairro: {formValues.bairro}</p>
-              <p>Estado:  {formValues.estado}</p>
-            </Card>
-          ))}
-        </BodyContainer>
-      ) : (
-        <div id="aviso">
-          <p>Não há lojas cadastradas! cadastre <Link to="/Cadastro-de-lojas">Aqui!</Link> </p>
-        </div>
-
-
-      )}
+      <BodyContainer>
+        {formValuesList.length !== 0 ? (
+          <Table>
+            <thead>
+              <tr>
+                <th>Razão social</th>
+                <th>CNPJ</th>
+                <th>Nome Fantasia</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Celular</th>
+                <th>Endereço</th>
+                <th>Número</th>
+                <th>Cidade</th>
+                <th>Bairro</th>
+                <th>Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {formValuesList.map((formValues, index) => (
+                <tr key={index}>
+                  <td>{formValues.razaoSocial}</td>
+                  <td>{formValues.cnpj}</td>
+                  <td>{formValues.nomeFantasia}</td>
+                  <td>{formValues.email}</td>
+                  <td>{formValues.telefone}</td>
+                  <td>{formValues.celular}</td>
+                  <td>{formValues.endereco}</td>
+                  <td>{formValues.numero}</td>
+                  <td>{formValues.cidade}</td>
+                  <td>{formValues.bairro}</td>
+                  <td>{formValues.estado} </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <div id="aviso">
+            <p>Não há lojas cadastradas! cadastre <Link to="/Cadastro-de-lojas">Aqui!</Link> </p>
+          </div>
+        )}
+      </BodyContainer>
     </>
   );
 }
---
+++ src/Components/style/Table-style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Table = styled.table`
+    padding: 0px 10px 0px 10px;
+    background-color:#2f76b4;
+    border-collapse: collapse;
+  
+    td {
+    border: 1px solid black;
+    }
+`;
+
+export default Table;
